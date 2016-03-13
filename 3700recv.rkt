def5ba2bad604a7563ad0b2ad0b7ed3ec3d5cb4b
Ty Coghlan <Ophirr33@zimbra.ccs.neu.edu>
2016-03-13 09:00:51 -0400

    initial commit

---
+++ 3700recv.rkt
@@ -0,0 +1,76 @@
+#! /proj/racket/plt-released/recent/bin/racket
+#lang racket/base
+
+(require json)
+(require racket/date)
+(require racket/string)
+(require racket/udp)
+;; log the given message
+(define (log str)
+  (write (string-append (now) " " str "\n") (current-error-port)))
+
+
+;; get the time as hour:minute:second:microsecond
+(define (now)
+  (define (upcast n)
+    (if (integer? n) (inexact->exact n)
+        (upcast (* 10 n))))
+  (let* ([milli (current-inexact-milliseconds)]
+         [d (current-date)]
+         [micro   (number->string (upcast(- milli (floor milli))))]
+         [seconds (number->string (date-second d))]
+         [minutes (number->string (date-minute d))]
+         [hours   (number->string (date-hour d))])
+    (string-append hours ":" minutes ":" seconds ":" micro)))
+
+; define values
+(define-values (msg-size timeout udp-ip udp-port)
+  (values 1500 30 "127.0.0.1" 0))
+
+; Set up the socket and bind to localhost/ephemeral port
+(define socket (udp-open-socket))
+(udp-bind! socket udp-ip udp-port)
+; Get port we bound to
+(define-values (local-address udp-p remote-address remote-port)
+  (udp-addresses socket #t))
+
+; Now listen for packets
+(let loop ([data-read (make-bytes msg-size)])
+  (define-values (bytes-received address port)
+    (udp-receive!* socket data-read 0 msg-size))
+  (if bytes-received
+      (with-handlers  ([exn:fail? (lambda (exn) (log "[recv corrupt packet]"))])
+        (let ([decoded (string->jsexpr data-read)])
+
+          ; If the EOF flag is set, exit
+          (when (hash-ref decoded "eof")
+            (begin (log "[completed]")
+                   (exit 0)))
+
+          (when (hash-ref decoded "data")
+            ;; IF we received data, we assume it's in order
+            (begin (log (string-append "[recv data] "
+                                       (number->string (hash-ref decoded "sequence"))
+                                       " ("
+                                       (number->string (string-length (hash-ref decoded "data")))
+                                       ") ACCEPTED (in-order)"))
+                   (write (hash-ref decoded "data"))
+                   (flush-output (current-output-port))))
+          
+          ;; If there is an ack, send next packet
+          (when (= (hash-ref decoded "ack") sequence)
+              (begin (log (string-append "[recv ack] " (number->string sequence)))
+                     ; Try to send next packet; break if no more data
+                     (when (not (send-next-packet))
+                       (void))))
+          
+          (define msg (jsexpr->string (hash "ack" (+ (hash-ref decoded "sequence")
+                                                     (string-length (hash-ref decoded "data"))))))
+          (log (string-append "ABOUT TO SEND " msg))
+          (udp-send-to* socket address port data-read)
+          (loop data-read)
+          ))
+      (begin (log "[error] timeout")
+             (exit -1))))
+
+(udp-close socket)
